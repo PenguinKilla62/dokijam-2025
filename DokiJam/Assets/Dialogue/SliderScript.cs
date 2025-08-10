f3fa9aa9359fa917f97ce7bf07f9886ecdd9e8c7
Penguin <penguinkilla62@gmail.com>
2025-08-09 18:16:31 -0700

    add example work mini game using dialogues

---
+++ DokiJam/Assets/Dialogue/SliderScript.cs
@@ -0,0 +1,54 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    //Values
+    public int RedValue;
+    public int BlueValue;
+    public int GreenValue;
+
+    //TMP_Text
+    public TMP_Text sliderRedText;
+    public TMP_Text sliderBlueText;
+    public TMP_Text sliderGreenText;
+
+    //Slider
+    public Slider redSlider;
+    public Slider blueSlider;
+    public Slider greenSlider;
+
+    public Image image;
+
+    private void changeImageColor()
+    {
+        image.color = new Color32((byte)RedValue, (byte)BlueValue, (byte)GreenValue, 255);
+    }
+
+    public void ChangeRedValue(float newValue)
+    {
+        RedValue = (int)newValue;
+        sliderRedText.text = $"{RedValue}";
+
+        changeImageColor();
+    }
+
+    public void ChangeBlueValue(float newValue)
+    {
+        BlueValue = (int)newValue;
+        sliderBlueText.text = $"{BlueValue}";
+
+        changeImageColor();
+    }
+
+    public void ChangeGreenValue(float newValue)
+    {
+        GreenValue = (int)newValue;
+        sliderGreenText.text = $"{GreenValue}";
+
+        changeImageColor();
+    }
+
+    
+}
---
+++ DokiJam/Assets/Dialogue/SubmitWork.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+
+public class SubmitWork : MonoBehaviour
+{
+
+    private InMemoryVariableStorage variableStorage;
+
+    // Keep track of GameObject Values
+    public SliderScript sliderGameObject;
+    public DialogueRunner dialogueRunner;
+
+    private int _redValue = 127;
+    private int _greenValue = 0;
+    private int _blueValue = 0;
+
+    private int _higherUpCurrentCount = 0;
+    public int higherUpMaxCount = 10;
+    private bool stopWork = false;
+
+    private enum ColorPick
+    {
+        Red,
+        Green,
+        Blue
+    };
+
+    public void Start()
+    {
+        variableStorage = FindFirstObjectByType<InMemoryVariableStorage>();
+        DetermineNextChange();
+        UpdateVariableStorage();
+        dialogueRunner.StartDialogue("WorkerStarting");
+        dialogueRunner.StartDialogue("WorkerSetMultipleColors");
+    }
+
+    public void DetermineNextChange()
+    {
+        var rnd = new System.Random();
+        int pickCount = rnd.Next(1, 3);
+        List<ColorPick> pickList = new List<ColorPick> { ColorPick.Red, ColorPick.Green, ColorPick.Blue };
+        List<ColorPick> chosenList = new List<ColorPick> { };
+
+        for (var i = 0; i < pickCount; i++)
+        {
+            var index = rnd.Next(pickList.Count);
+            chosenList.Add(pickList[index]);
+            pickList.RemoveAt(index);
+        }
+
+        foreach (var pick in chosenList)
+        {
+            switch (pick)
+            {
+                case ColorPick.Red:
+                    _redValue = rnd.Next(0, 255);
+                    break;
+                case ColorPick.Green:
+                    _greenValue = rnd.Next(0, 255);
+                    break;
+                case ColorPick.Blue:
+                    _blueValue = rnd.Next(0, 255);
+                    break;
+            }
+        }
+
+    }
+
+    private bool MeetsCurrentChange()
+    {
+        bool result = true;
+
+        if (_redValue != sliderGameObject.RedValue || _greenValue != sliderGameObject.GreenValue || _blueValue != sliderGameObject.BlueValue)
+        {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public void UpdateVariableStorage()
+    {
+        variableStorage.SetValue("$red_amount", _redValue);
+        variableStorage.SetValue("$green_amount", _greenValue);
+        variableStorage.SetValue("$blue_amount", _blueValue);
+    }
+
+    private bool StopWork()
+    {
+        return _higherUpCurrentCount >= higherUpMaxCount && sliderGameObject.BlueValue == 127
+            && sliderGameObject.RedValue == 127 && sliderGameObject.GreenValue == 127;
+    }
+
+    public void CheckWork()
+    {
+        if (MeetsCurrentChange() == false)
+        {
+            dialogueRunner.StartDialogue("WorkerFailedSelfReview");
+            dialogueRunner.StartDialogue("WorkerSetMultipleColors");
+        }
+        else
+        {
+            if (!stopWork)
+            {
+                dialogueRunner.StartDialogue("WorkerHigherUpReviewing");
+            }
+
+            if (StopWork())
+            {
+                stopWork = true;
+                dialogueRunner.StartDialogue("WorkerFinished");
+            }
+            else
+            {
+                if (_higherUpCurrentCount >= higherUpMaxCount)
+                {
+                    _redValue = 127;
+                    _greenValue = 127;
+                    _blueValue = 127;
+                }
+                else
+                {
+                    DetermineNextChange();
+                    _higherUpCurrentCount += 1;
+                }
+                UpdateVariableStorage();
+                dialogueRunner.StartDialogue("WorkerDisgruntled");
+                dialogueRunner.StartDialogue("WorkerSetMultipleColors");
+            }
+        }
+    }
+}
