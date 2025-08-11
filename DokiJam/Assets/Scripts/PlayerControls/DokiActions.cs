533385d54e4c3d8f0306b897d7de96a4dd33360e
Haocheng Mai <lightwizard42@gmail.com>
2025-08-10 22:48:34 -0700

    attacks

--- DokiJam/Assets/Scripts/PlayerControls/DokiActions.cs
+++ DokiJam/Assets/Scripts/PlayerControls/DokiActions.cs
@@ -20,7 +20,7 @@ public class DokiActions : MonoBehaviour
 
     [Header("References")]
     public Rigidbody2D rb;
-    public GameObject normalDragoonPrefab;
+    public NormalDragoon normalDragoonData;
     public GameObject longDragoonPrefab;
     public GameObject beegDragoonPrefab;
 
@@ -109,7 +109,7 @@ public class DokiActions : MonoBehaviour
             Debug.Log("Max number of dragoons reached");
             return; // Don't spawn more dragoons than allowed
         }
-        var tossedDragoon = Instantiate(normalDragoonPrefab, transform.position, Quaternion.identity);
+        var tossedDragoon = Instantiate(normalDragoonData.dragoonPrefab, transform.position, Quaternion.identity);
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(inputActions.Player.MousePos.ReadValue<Vector2>());
         Vector2 direction = (mousePosition - rb.position).normalized;
         tossedDragoon.GetComponent<Rigidbody2D>().linearVelocity = (direction * speed) + rb.linearVelocity;
--- DokiJam/Assets/Scripts/ScriptableObjects/NormalDragoon.cs
+++ DokiJam/Assets/Scripts/ScriptableObjects/NormalDragoon.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NormalDragoon", menuName = "Scriptable Objects/NormalDragoon")]
 public class NormalDragoon : ScriptableObject
 {
-    
+    public int damage = 10;
+    public float speed = 5f;
+
+    [Header("Prefab Reference")]
+    public GameObject dragoonPrefab;
 }
